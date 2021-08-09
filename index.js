a85fbf8fa3c2a4a1618df8b087f59603a823a7c7
MicroCloudService <83729147+MicroCloudService@users.noreply.github.com>
2021-08-09 15:48:53 +1200

    Adding LowCodeAzure Branding

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    const myURL = new URL('https://lowcodeazure.com/');
+    
+    response.write(myURL);
+    response.end("Hello World From LowCodeAzure!");
 });
 
 const port = process.env.PORT || 1337;
